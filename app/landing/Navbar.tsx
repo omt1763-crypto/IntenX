c81224576a89d2985edbdf0257283c3749f207dd
omt1763-crypto <your-actual-email@gmail.com>
2025-12-30 16:46:22 +0530

    Adjust logo to 50px

--- app/landing/Navbar.tsx
+++ app/landing/Navbar.tsx
@@ -33,7 +33,7 @@ const Navbar = () => {
           <img 
             src="/intenx-logo.png" 
             alt="IntenX" 
-            style={{ width: '120px', height: 'auto' }}
+            style={{ width: '50px', height: 'auto' }}
           />
         </div>
 
