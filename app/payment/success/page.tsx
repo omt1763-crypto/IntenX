416989fac5bf5f3a1d4415590f793325b08bed9e
omt91 <omt91560A@gmail.com>
2026-01-24 14:20:15 +0530

    fix: Mark payment pages as dynamic to prevent prerender errors

--- app/payment/success/page.tsx
+++ app/payment/success/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { useSearchParams } from 'next/navigation';
 import { CheckCircle } from 'lucide-react';
 import Link from 'next/link';
